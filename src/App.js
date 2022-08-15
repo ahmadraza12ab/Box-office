55f657d8cd54b538a342a8c1d3a1775ca6433b3a
Ahmad Raza <ahmadraza12ab@gmail.com>
2022-08-16 01:37:15 +0530

    added navar and created pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Navs from './components/Navs';
+import Home from './components/pages/Home';
+import Starred from './components/pages/Starred';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/">
-        This is home page
-      </Route>
-      <Route exact path="/starred">
-        This is starred
-      </Route>
-      <Route>This is 404 page</Route>
-    </Switch>
+    <div>
+      <Navs />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
+        <Route>
+          <div>Not found</div>
+        </Route>
+      </Switch>
+    </div>
   );
 }
 
---
+++ src/components/pages/Starred.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Starred() {
+  return <div>This is starred</div>;
+}
+
+export default Starred;
