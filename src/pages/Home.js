af206d2e06f492485582a721fb9ed7f13d31c632
Ahmad Raza <ahmadraza12ab@gmail.com>
2022-08-16 13:29:41 +0530

    input and fetch resul from api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,44 @@
-import React from 'react';
+/* eslint-disable no-console */
+import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
 
 function Home() {
-  return <MainPageLayout>This is home</MainPageLayout>;
+  const [input, setInput] = useState('');
+  const onInputChange = ev => {
+    // eslint-disable-next-line no-console
+    console.log(ev.target.value);
+    setInput(ev.target.velue);
+  };
+  const onSearch = () => {
+    // https://api.tvmaze.com/search/shows?q=men
+
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+      .then(r => r.json())
+      .then(result => {
+        // eslint-disable-next-line no-console
+        console.log(result);
+      });
+  };
+  const onKeyDown = ev => {
+    if (ev.keyCode === 13) {
+      onSearch();
+    }
+    console.log(ev.keyCode);
+  };
+
+  return (
+    <MainPageLayout>
+      <input
+        type="text"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+    </MainPageLayout>
+  );
 }
 
 export default Home;
